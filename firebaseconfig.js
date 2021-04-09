ea4f4137b8d4b55a97700d98c364e6bf1bcd552f
Md.Mohtasim Shariar <onim04@gmail.com>
2021-04-10 00:48:35 +0600

    firebase config

---
+++ firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACK4nwIQuBzfDLLZDGXmDLt0QuPzT5Cug",
+    authDomain: "solar-shop-3eee1.firebaseapp.com",
+    projectId: "solar-shop-3eee1",
+    storageBucket: "solar-shop-3eee1.appspot.com",
+    messagingSenderId: "888487987654",
+    appId: "1:888487987654:web:5ff8783114369550b6cac7",
+    measurementId: "G-BGRJ6V3FV2"
+  };
+
+export default firebaseConfig;
