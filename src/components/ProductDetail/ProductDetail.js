e11e645fcc74ce7e2b74bafb3cf779cd1e822084
Md.Mohtasim Shariar <onim04@gmail.com>
2021-04-10 00:40:49 +0600

    first commit

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useParams } from 'react-router';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+
+
+const ProductDetail = () => {
+    const {productkey} =useParams();
+    const product = fakeData.find(pd =>pd.key===productkey);
+    return (
+        <div>
+            <h1>Your Product Detail</h1>
+            <Product showAddToCart={false} product={product}></Product>
+        </div>
+    );
+};
+
+export default ProductDetail;
